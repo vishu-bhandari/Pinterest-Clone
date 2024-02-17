c488efff6580c9cceb9b4309b4c858979b416eff
vishu bhandari <deevishu1234@gmail.com>
2024-02-17 15:53:54 +0530

    multer added

--- routes/index.js
+++ routes/index.js
@@ -1,69 +1,71 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const userModel=require("./users");
-const postModel=require("./post");
-const passport = require('passport');
-const localStrategy=require("passport-local");
+const userModel = require("./users");
+const postModel = require("./post");
+const passport = require("passport");
+const upload = require("./multer");
+const localStrategy = require("passport-local");
 passport.use(new localStrategy(userModel.authenticate()));
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
 
+router.get("/login", function (req, res, next) {
+  res.render("login", { error: req.flash("error") });
+});
 
-router.get('/login',function(req,res,next){
-  
-  res.render('login',{error:req.flash('error')});
-})
+router.get("/feed", function (req, res) {
+  res.render("feed");
+});
 
-router.get('/feed',function(req,res){
-  res.render('feed');
-})
+router.post("/upload", upload.single("file"), function (req, res) {
+  if (!req.file) {
+    return res.status(404).send("no files were given");
+  }
+  res.send("file upload successful");
+});
 
-router.get("/profile",isLoggedIn,async function(req,res,next){
+router.get("/profile", isLoggedIn, async function (req, res, next) {
   const user = await userModel.findOne({
-    username:req.session.passport.user
+    username: req.session.passport.user,
   });
-  res.render('profile', {user});
-  
-})
-
-router.post("/register", function(req, res) {
-  const {username,email,fullname}=req.body;
-  const userData = new userModel({username,email,fullname});
-  userModel.register(userData,req.body.password)
-  .then(function(){
-    passport.authenticate("local")(req,res,function(){
-      res.redirect('/profile');
-    })
-
-  })
-})
-
-
-router.post("/login",passport.authenticate("local",{
-  successRedirect:"/profile",
-  failureRedirect:"/login",
-  failureFlash:true
-}),function(req,res){
+  res.render("profile", { user });
 });
 
-router.get("/logout",function(req,res){
-  req.logout(function(err){
-    if(err){
+router.post("/register", function (req, res) {
+  const { username, email, fullname } = req.body;
+  const userData = new userModel({ username, email, fullname });
+  userModel.register(userData, req.body.password).then(function () {
+    passport.authenticate("local")(req, res, function () {
+      res.redirect("/profile");
+    });
+  });
+});
+
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/profile",
+    failureRedirect: "/login",
+    failureFlash: true,
+  }),
+  function (req, res) {}
+);
+
+router.get("/logout", function (req, res) {
+  req.logout(function (err) {
+    if (err) {
       return next(err);
     }
-    res.redirect('/login');
-  })
-})
+    res.redirect("/login");
+  });
+});
 
-
-function isLoggedIn(req,res,next){
-  if(req.isAuthenticated()) return next();
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) return next();
   res.redirect("/login");
 }
-
-
 
 module.exports = router;
